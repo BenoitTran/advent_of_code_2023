d896cad3d6c40793f2fe853b15844f1959210139
Tran <benoit.tran@tutanota.com>
2023-12-05 05:07:28 +0100

    regex improvement

--- 2/main.jl
+++ 2/main.jl
@@ -5,13 +5,24 @@ lines = open("2/input.txt") do file
     [replace(l, r"Game \d+: " => "") for l in eachline(file)]
 end
 
+# Part 1
 sum = Ref(0)
 for (k,l) in enumerate(lines)
-    reds = [parse(Int, match(r"\d+", m.match).match) for m in eachmatch(r"\d+\s+red", l)]
-    greens = [parse(Int, match(r"\d+", m.match).match) for m in eachmatch(r"\d+\s+green", l)]
-    blues = [parse(Int, match(r"\d+", m.match).match) for m in eachmatch(r"\d+\s+blue", l)]
+    reds = [parse(Int, m[1]) for m in eachmatch(r"(\d+)\s+red", l)]
+    greens = [parse(Int, m[1]) for m in eachmatch(r"(\d+)\s+green", l)]
+    blues = [parse(Int, m[1]) for m in eachmatch(r"(\d+)\s+blue", l)]
     if all(i -> i ≤ max_r, reds) && all(i -> i ≤ max_g, greens) && all(i -> i ≤ max_b, blues)
         sum[] += k
     end
 end
-println(sum[])
+println("Part 1: ", sum[])
+
+# Part 2
+sum_power = Ref(0)
+for (k,l) in enumerate(lines)
+    red = maximum([parse(Int, m[1]) for m in eachmatch(r"(\d+)\s+red", l)])
+    green = maximum([parse(Int, m[1]) for m in eachmatch(r"(\d+)\s+green", l)])
+    blue = maximum([parse(Int, m[1]) for m in eachmatch(r"(\d+)\s+blue", l)])
+    sum_power[] += red * green * blue
+end
+println("Part 2: ", sum_power[])
